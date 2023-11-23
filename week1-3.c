a85bda702e3897a3606d5841f3bdea445d93a80a
mrliber8 <patricktenbrinke@gmail.com>
2023-11-23 11:30:23 +0100

    Sluit nu pas als je een negatief getal invoert

--- week1-3.c
+++ week1-3.c
@@ -6,9 +6,21 @@ int main()
     int testIntiger;
     printf("Make an option between the following three choises: \n");
     printf("1. If you enter a negative number, the program will close \n");
-    printf("2. If you enter a zero, 10 zero's will be displayed \n");
-    printf("3. If you enter a positive number, hello world will be printed \n");
+    printf("2. If you enter a positive number, 10 lines of stars and dashes will be displayed \n");
+    printf("3. If you enter a zero, hello world will be printed \n");
     printf("Now enter an integer:  ");
+
+    while (1)
+    {
+        if (readAndDisplay() == 0){
+            break;
+        }
+    }
+    
+}
+
+int readAndDisplay() {
+    int testIntiger;
     scanf("%d", &testIntiger);
     printf("number = %d \n", testIntiger);
 
@@ -22,6 +34,9 @@ int main()
     }
     else if (testIntiger == 0)
     { 
-        printf("Hello World");
+        printf("Hello World \n");
     }
+
+    printf("Now enter another integer:  ");
+    return 1;
 }
