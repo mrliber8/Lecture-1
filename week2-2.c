2ce54f46859b1ab9f811b3dcfb1fd78c718204e5
mrliber8 <patricktenbrinke@gmail.com>
2023-11-24 11:27:38 +0100

    Print de lengte zonder spaces

--- week2-2.c
+++ week2-2.c
@@ -2,6 +2,8 @@
 Schrijf een C programma die de gebruiker vraag om een zin.
 Na invoer wordt de lengte van deze ingevoerde zin uitgeprint (in aantal chars).
 Lever het bestand in met de naam: “week2-2.c”.
+
+Strings.
 */
 
 #include <stdio.h>
@@ -9,23 +11,30 @@ Lever het bestand in met de naam: “week2-2.c”.
 int main(int argc, char const *argv[])
 {
 
-    char sentance[50];
+    //Initialiseer een char die de zin kan bevatten.
+    char sentance[100];
+
+    //Vraag de user om input
     printf("Enter a sentance, max 100 characters: ");
-    fgets(sentance, sizeof(sentance), stdin);  // read string
 
-    sentance[strcspn(sentance, "\n")] = 0;
+    //Lees de string in
+    fgets(sentance, sizeof(sentance), stdin); 
+    //scanf("%s",sentance); 
 
+    //print de lengte van de complete zin
     printf("De lengte van de ingevoerde zin is: %lu karakters.\n", strlen(sentance));
 
+    //Bepaal de lengte zonder spaces en end
+    int sentanceLength = strlen(sentance);
+    for (int i = 0; i < strlen(sentance); i++)
+    {
+        if (sentance[i] == ' ' || sentance[i] == '\0' )
+        {
+            sentanceLength += -1;
+        }
+    }
 
+    printf("De lengte zonder spaces en trailing end is: %lu kartakters.\n", sentanceLength);
 
-    //printf("Sentrance is: ");
-    //puts(sentance);
-
-
-    //scanf("%S", sentance);
-    //printf("The sentace was %S.", sentance);
     return 0;
 }
-
-
